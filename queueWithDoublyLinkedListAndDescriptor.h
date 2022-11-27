2c2206a02f5f1c38fc07da88e39304c9af51f9e6
Jessica Perdomo <j.perdomoalves@gmail.com>
2022-11-27 17:17:25 -0300

    Adding queue with doubly linked list

---
+++ queueWithDoublyLinkedListAndDescriptor.h
@@ -0,0 +1,58 @@
+struct TpQueue{
+	int elem;
+	TpQueue *before,*next;
+};
+
+struct Descriptor{
+	TpQueue *start,*end;
+};
+
+void init(Descriptor &desc){
+	desc.start=NULL;
+	desc.end=NULL;
+}
+
+char isEmpty(Descriptor &desc){
+	if(desc.start == NULL){
+		return 1;
+	}
+	else{
+		return 0;
+	}
+}
+
+void push(int e, Descriptor &desc){
+	TpQueue *NC = new TpQueue();
+	NC->elem = e;
+	NC->next = NULL;
+	NC->before = NULL;
+	
+	if(isEmpty(desc.start)){
+		desc.start=NC;
+		desc.end=NC;
+	}
+	else{
+		NC->next=desc.start;
+		desc.start->before=NC;
+		desc.start=NC;
+	}
+}
+
+void pop(Descriptor &desc){
+	TpQueue *aux=desc.start;
+	
+	if(aux->next == NULL){
+		delete(aux);
+		desc.start = NULL;
+		desc.end = NULL;
+	}
+	else{
+		while(aux->next != NULL){
+			aux=aux->next;
+		}
+		
+		desc.end = aux->before;
+		delete(aux);
+		desc.end->prox=NULL;
+	}
+}
